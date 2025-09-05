8bfb970fbe78b1c41fcfe5ef21efbb64ff9d5950
cicerocarlos-ufal <112500002+cicerocarlos-ufal@users.noreply.github.com>
2025-09-05 09:23:48 -0300

    Add files via upload

--- R/runMenuAgriR.R
+++ R/runMenuAgriR.R
@@ -7,21 +7,5 @@
 runMenuAgriR <- function() {
   appDir <- system.file("shiny", package = "AgriR")
   if (appDir == "") stop("App directory not found in the package")
-  
-  # tenta determinar navegador
-  browser_path <- getOption("browser")
-  
-  # função de fallback caso browser não esteja definido
-  launch_browser <- TRUE
-  if (is.null(browser_path) || browser_path == "") {
-    message("No browser detected. You can manually open the app at the URL printed below.")
-    launch_browser <- FALSE
-  }
-  
-  # executa o app
-  shiny::runApp(
-    appDir,
-    launch.browser = launch_browser
-  )
 }
 
