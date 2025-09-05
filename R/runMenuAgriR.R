959df3a12eaa2d7992927d848e95edcf410ced39
cicerocarlos-ufal <112500002+cicerocarlos-ufal@users.noreply.github.com>
2025-09-05 09:09:33 -0300

    Add files via upload

--- R/runMenuAgriR.R
+++ R/runMenuAgriR.R
@@ -1,11 +1,27 @@
-#' Executa o app Shiny menu_agriR
-#'
-#' Esta função abre a aplicação Shiny incluída no pacote AgriR.
+#' Run the Shiny app Menu AgriR
+#' 
+#' Opens the Shiny app included in the AgriR package.
+#' If a browser is available, it will launch automatically.
+#' Otherwise, the URL will be printed for manual access.
 #' @export
 runMenuAgriR <- function() {
   appDir <- system.file("shiny", package = "AgriR")
-  if (appDir == "") {
-    stop("Não foi possível localizar o app Shiny no pacote AgriR.")
+  if (appDir == "") stop("App directory not found in the package")
+  
+  # tenta determinar navegador
+  browser_path <- getOption("browser")
+  
+  # função de fallback caso browser não esteja definido
+  launch_browser <- TRUE
+  if (is.null(browser_path) || browser_path == "") {
+    message("No browser detected. You can manually open the app at the URL printed below.")
+    launch_browser <- FALSE
   }
-  shiny::runApp(appDir)
+  
+  # executa o app
+  shiny::runApp(
+    appDir,
+    launch.browser = launch_browser
+  )
 }
+
